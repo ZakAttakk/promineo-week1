7643a90488f31c4fd5a92a8c151e56ccffc70593
ZakAttakk <zjbrewer@gmail.com>
2022-12-06 20:35:16 -0500

    another commit

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 console.log("It works!")
 
+// This is a comment
+var myName = "Zach"
+let lastName = "Brewer"
+const middleName = "Joseph"
+var age = "40"
+var otherAge = "40"
+var isCool = true
+
+
+console.log(myName + " " + middleName + lastName)
+
+
 
